1b42e0bd016364826b0aa2e5423339663fa63434
jvrcavalcanti <jonnyvictor01@gmail.com>
2020-06-02 20:03:30 -0300

    Version favicon

--- src/Template.php
+++ src/Template.php
@@ -114,7 +114,7 @@ class Template
             <head>
                 <meta charset='{$charset}'>
                 <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-                <link rel='shortcut icon' type='image/x-icon' href='favicon.ico' />
+                <link rel='shortcut icon' type='image/x-icon' href='favicon.ico?v=" . md5(microtime(true)) . "' />
                 <title>{$title}</title>
             <style>
             {$template}
